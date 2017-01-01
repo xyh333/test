50e5acd36f21beeba785da3f9d6db71432d88ceb
xyh333 <yx101@duke.edu>
2017-01-01 02:26:47 -0500

    remove

--- rail_project/test_app/db/migrate/20161230033501_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :articles, :description, :text
-    add_column :articles, :created_at, :datatime
-    add_column :articles, :updated_at, :datetime
-  end
-end
